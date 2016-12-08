52ba59bdf547ffea3673e6304fab16f47a98c125
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2016-12-08 14:37:06 -0600

    Made a mesh constructor

--- src/main/java/com/dustinroepsch/SoftwareRenderer/Engine/Model/Mesh.java
+++ src/main/java/com/dustinroepsch/SoftwareRenderer/Engine/Model/Mesh.java
@@ -1,7 +1,11 @@
 package com.dustinroepsch.SoftwareRenderer.Engine.Model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Mesh is a 3D model.
@@ -9,7 +13,44 @@ import java.util.ArrayList;
 public class Mesh {
     private ArrayList<Face> faces;
 
-    public Mesh(File file) {
+    /**
+     * Creates a Mesh from a Wavefront Obj file.
+     *
+     * @param file The Wavefront Obj file.
+     * @throws FileNotFoundException File must be readable
+     */
+    public Mesh(File file) throws FileNotFoundException {
+        faces = new ArrayList<Face>();
+        Scanner fileScanner = new Scanner(file);
+        //matches the pattern v 0.608654 -0.568839 -0.416318
+        //puts each float into a capture group.
+        Pattern verticePattern = Pattern.compile("v\\s*([-+]?[0-9]*\\.?[0-9]+)\\s*([-+]?[0-9]*\\.?[0-9]+)\\s*([-+]?[0-9]*\\.?[0-9]+)");
+        //matches the pattern f 1193/1240/1193 1180/1227/1180 1179/1226/1179
+        //puts each number into a capture group
+        Pattern facePattern = Pattern.compile("f\\s*(\\d*)\\/(\\d*)\\/(\\d*)\\s*(\\d*)\\/(\\d*)\\/(\\d*)\\s*(\\d*)\\/(\\d*)\\/(\\d*)");
 
+        ArrayList<Vertice> verts = new ArrayList<Vertice>();
+
+        while (fileScanner.hasNextLine()) {
+            String currentLine = fileScanner.nextLine();
+            Matcher verticeMatcher = verticePattern.matcher(currentLine);
+            if (verticeMatcher.matches()) {
+                verts.add(new Vertice(
+                        Float.valueOf(verticeMatcher.group(1)),
+                        Float.valueOf(verticeMatcher.group(2)),
+                        Float.valueOf(verticeMatcher.group(3))
+                ));
+            }
+            Matcher faceMatcher = facePattern.matcher(currentLine);
+            if (faceMatcher.matches()) {
+                faces.add(
+                  new Face(
+                          verts.get(Integer.parseInt(faceMatcher.group(1))),
+                          verts.get(Integer.parseInt(faceMatcher.group(4))),
+                          verts.get(Integer.parseInt(faceMatcher.group(7)))
+                  )
+                );
+            }
+        }
     }
 }
